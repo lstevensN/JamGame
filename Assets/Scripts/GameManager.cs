69b98f72bc70d7571e9baa9266c5fa1d4b8ffa24
lstevensN <lstevens@student.neumont.edu>
2025-08-09 11:25:16 -0600

    Buggle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
+
     [SerializeField] GameObject PlayerRef;
     [SerializeField] GameObject PlayerSpawnPoint;
     [SerializeField] bool SpawnPlayer = true;
@@ -20,24 +23,31 @@ public class GameManager : MonoBehaviour
     bool PrevPlayerGrabbed = false;
     bool PlayerGrabbed = false;
 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Subscribe to scene load event
+        SceneManager.sceneLoaded += OnSceneLoad;
 
-        if(SpawnPlayer)
-        {
-            Player = Instantiate(PlayerRef, new Vector2(PlayerSpawnPoint.transform.position.x, PlayerSpawnPoint.transform.localPosition.y), PlayerSpawnPoint.transform.rotation);
-        }
-        
-        if(SpawnDev)
-        {
-            Dev = Instantiate(DevRef, new Vector2(DevSpawnPoint.transform.position.x, DevSpawnPoint.transform.localPosition.y), DevSpawnPoint.transform.rotation);
-            Dev.GetComponent<DevHand>().player = Player;
-            Dev.GetComponent<DevHand>().playerSpawn = PlayerSpawnPoint;
-        }
+        // Spawn Player
+        Player = Instantiate(PlayerRef, new Vector2(PlayerSpawnPoint.transform.position.x, PlayerSpawnPoint.transform.localPosition.y), PlayerSpawnPoint.transform.rotation);
     }
 
- 
+    void OnDestroy()
+    {
+        // Unsubscribe to scene load event
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
 
     // Update is called once per frame
     void Update()
@@ -46,7 +56,7 @@ public class GameManager : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.R)) Respawn = true;
 
         //player grabbed
-        if(Dev.GetComponent<DevHand>().playerCaught != PlayerGrabbed)
+        if(Dev && Dev.GetComponent<DevHand>().playerCaught != PlayerGrabbed)
         {
             PlayerGrabbed = Dev.GetComponent<DevHand>().playerCaught;
             if (PlayerGrabbed)
@@ -58,6 +68,12 @@ public class GameManager : MonoBehaviour
                 Player.GetComponent<Rigidbody2D>().gravityScale = 1;
             }
         }
+
+        // Adjust spawn point (kinda silly, but it works! :D)
+        PlayerSpawnPoint.transform.position = new Vector2(
+            Player.transform.position.x > 0 ? -15 : 15,
+            Player.transform.position.y > 0 ? 2 : -5
+            );
     }
  
     private void LateUpdate()
@@ -81,5 +97,22 @@ public class GameManager : MonoBehaviour
     private void OnPlayerPickedUp()
     {
         print("Player Picked Up");
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        // Get level "data", set SpawnPlayer & SpawnDev variables accordingly
+
+        if (SpawnPlayer)
+        {
+            Player = Instantiate(PlayerRef, new Vector2(PlayerSpawnPoint.transform.position.x, PlayerSpawnPoint.transform.localPosition.y), PlayerSpawnPoint.transform.rotation);
+        }
+
+        if (SpawnDev)
+        {
+            Dev = Instantiate(DevRef, new Vector2(DevSpawnPoint.transform.position.x, DevSpawnPoint.transform.localPosition.y), DevSpawnPoint.transform.rotation);
+            Dev.GetComponent<DevHand>().player = Player;
+            Dev.GetComponent<DevHand>().playerSpawn = PlayerSpawnPoint;
+        }
     }
 }
