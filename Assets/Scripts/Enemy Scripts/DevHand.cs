36f75d872d5c5707395e218a808283cb917fe551
Lace-Gang <rhall@student.neumont.edu>
2025-08-08 20:27:58 -0600

    Obligatory Save

--- Assets/Scripts/Enemy Scripts/DevHand.cs
+++ Assets/Scripts/Enemy Scripts/DevHand.cs
@@ -25,7 +25,7 @@ public class DevHand : MonoBehaviour
     {
         if (playerCaught)
         {
-            this.transform.position = Vector2.MoveTowards(this.transform.position, playerSpawn.transform.position, 0.02f);
+            this.transform.position = Vector2.MoveTowards(this.transform.position, playerSpawn.transform.position, 0.01f);
         }
         else this.transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, 0.001f);
     }
