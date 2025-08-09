6811ff99e41412b8ccdec5491e4fd2355b5abdb5
Lace-Gang <rhall@student.neumont.edu>
2025-08-09 11:00:36 -0600

    Enemy Start

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    GameObject player;
+    bool playerSpotted = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,26 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerSpotted)
+        {
+            if(player.transform.position.x >)
+            Attack();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject;
+            playerSpotted = true;
+        }
+    }
+
+
+    private void Attack()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+
     }
 }
